eae0db91fccb5b9aba2300c1c1a614dc7c0c08a1
Matias Wallenius <MatiWall@live.dk>
2024-12-18 17:04:55 +0100

    added index.ts

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { AppConfig, createAppConfig } from "./AppConfig";
+import configSchema from "./schema";
+import {AppConfigProvider, useAppConfig} from './AppConfigProvider'
+
+export {
+    configSchema,
+
+    AppConfig,
+    createAppConfig,
+
+    AppConfigProvider, 
+    useAppConfig
+
+}
